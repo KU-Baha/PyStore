8f18468de6910048b717666e30fe319d0009c464
KU-Baha <kulbarakovich@gmail.com>
2023-03-01 20:06:22 +0600

    Протестировал модельки

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-from shop.models import Product
+from shop.models import Product, Category
 
-Product()
+phone = Category('Phones')
+
+iphone15 = Product('Iphone 15 black', 'Apple', 'iphone 15', 990, 'black', 256, category=phone)
+samsung = Product('Samsung S23', 'Samsung', 'S23', 850, 'white', 512, category=phone)
+
+print(iphone15.__dict__)
+print(samsung.__dict__)
